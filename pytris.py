69abdc21fe12c91f47e67a5cecb6bc438956a15d
toshkeen <slavinant@gmail.com>
2019-10-26 18:25:30 +0300

    Add empty functions; fix SHAPE dict generation

--- pytris.py
+++ pytris.py
@@ -11,6 +11,8 @@ BLOCK_SIZE = cfg.getint('Options', 'blocksize')
 BOARD_H = cfg.getint('Options', 'boardheight')
 BOARD_W = cfg.getint('Options', 'boardwidth')
 FPS = cfg.getint('Options', 'fpscap')
+SCREEN_H = BLOCK_SIZE * BOARD_H
+SCREEN_W = BLOCK_SIZE * BOARD_W
 
 def start_gui():
     # GUI/Menu function for customizing game and running it
@@ -21,31 +23,54 @@ def start_gui():
 def run_game():
     # The main game runtime function
 
+    def choose_shape():
+        pass
+
+    def draw_shape():
+        pass
+
+    def rotate_shape():
+        pass
+
+    def get_size():
+        pass
+
+    def save_shape():
+        pass
+
+    def shape_below():
+        pass
+
+    def left_blocked():
+        pass
+
+    def right_blocked():
+        pass
+
     # Decode shape data into matrix form and save shapes into dict
     SHAPES = dict()
     for item in cfg.items('Shapes'):
         SHAPES[item[0].upper()] = list()
         for row in item[1].split(','):
-            SHAPES[item[0].upper()].append(row)    
+            SHAPES[item[0].upper()].append(list(row))  
 
     # Create an empty matrix - playing board
     GRID = list()
     for y in range(BOARD_H):
         GRID.append(list())
         for x in range(BOARD_W):
-            GRID[y].append(0)
-
-    # Set up local variables
-    SCREEN_H = BLOCK_SIZE * BOARD_H
-    SCREEN_W = BLOCK_SIZE * BOARD_W
+            GRID[y].append('0')
 
     # Pygame initialization
     pg.init()
     scr = pg.display.set_mode((SCREEN_W, SCREEN_H))
     pg.display.set_caption("PYTRIS")
     clock = pg.time.Clock()
-    is_running = True
 
+    # Set up local variables
+    dt = clock.tick(FPS) # Will be used for locking FPS time
+
+    is_running = True
     while is_running:
         # Input/controls 
         event = pg.event.poll()
@@ -54,7 +79,7 @@ def run_game():
         
         # Update display, run next frame
         pg.display.flip()
-        clock.tick(FPS)
+        dt = clock.tick(FPS)
 
     pg.quit()
 
